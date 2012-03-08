8ec3a189273a4724cef427b1dd6e939a943b6039
hammondjn <jessica@mobiusconsortium.org>
2012-03-08 09:35:38 -0600

    Adjusted the redirect message to reflect the user's scope

--- domainredirect.js
+++ domainredirect.js
@@ -22,11 +22,29 @@ if(href.indexOf("missouri.edu") != -1 ){
     window.location = redirectUrl; 
 }else{}
 
+var s = /searchscope=\d*/
+var t = /~S\d*/
+if (href.match(s)){
+  var searchscope = href.match(s)
+  var scope = searchscope[0].substring(12);
+}else if (href.match(t)){
+  var tilde = href.match(t)
+  var scope = tilde[0].substring(2);
+}else {
+  var scope;
+}
+
+if (scope == undefined || scope == 0){
+  var newURL = searchmobius
+}else {
+  var newURL = searchmobius + "/search~S" + scope
+}
+
 if (href.indexOf("&redirected") != -1 ) {
-    var redirectMessage = "<p>You've been redirected from swan.mobius.umsystem.edu or swan.missouri.edu. Starting in June of 2012, those addresses will no longer work.</p><p>Please update your bookmarks to <a href='http://swan.searchmobius.org'>http://swan.searchmobius.org</a>.</p>"
+    
+    var redirectMessage = "<p>You've been redirected from swan.mobius.umsystem.edu or swan.missouri.edu. Starting in June of 2012, those addresses will no longer work.</p><p>Please update your bookmarks to <a href='" + newURL + "'>" + newURL + "</a>.</p>"
 
     $(document).ready(function () {
         $('#redirectMessage').html(redirectMessage);
     });  
 }
-
