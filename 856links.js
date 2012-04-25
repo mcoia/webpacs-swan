27f1181f4557fb6c34199b818c550eb8d04cd6cd
hammondjn <jessica@mobiusconsortium.org>
2012-04-25 15:18:03 -0500

    Added an extra 856 string to SBU and MSSU

--- 856links.js
+++ 856links.js
@@ -1,9 +1,9 @@
 var scopeStrings = new Array ();
 scopeStrings[1] = new Array ("Crowder");
 scopeStrings[2] = new Array ("drury");
-scopeStrings[3] = new Array ("spivalib");
+scopeStrings[3] = new Array ("spivalib", "mssu");
 scopeStrings[4] = new Array ("my.otc");
-scopeStrings[5] = new Array ("sbuniv");
+scopeStrings[5] = new Array ("sbuniv", "SBU");
 scopeStrings[6] = new Array ("missouristate"); 
 scopeStrings[9] = new Array ("gobbc");
 scopeStrings[12] = new Array ("online.forest");
