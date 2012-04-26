902466697ee9abcbbb3f560cde558e514b478ef0
hammondjn <jessica@mobiusconsortium.org>
2012-04-26 13:37:25 -0500

    Hides the external link box if the other scopes have links, but the scope in question doesn't

--- 856links.js
+++ 856links.js
@@ -1,33 +1,49 @@
 function hideLinks(){
     
-var scopeStrings = new Array ();
-scopeStrings[1] = new Array ("Crowder");
-scopeStrings[2] = new Array ("drury");
-scopeStrings[3] = new Array ("spivalib", "mssu");
-scopeStrings[4] = new Array ("my.otc");
-scopeStrings[5] = new Array ("sbuniv", "SBU");
-scopeStrings[6] = new Array ("missouristate"); 
-scopeStrings[9] = new Array ("gobbc");
-scopeStrings[12] = new Array ("online.forest");
+    var stringsToHide = new Array ();
+    stringsToHide[1] = new Array ("Crowder");
+    stringsToHide[2] = new Array ("drury");
+    stringsToHide[3] = new Array ("spivalib", "mssu");
+    stringsToHide[4] = new Array ("my.otc");
+    stringsToHide[5] = new Array ("sbuniv", "SBU");
+    stringsToHide[6] = new Array ("missouristate"); 
+    stringsToHide[9] = new Array ("gobbc");
+    stringsToHide[12] = new Array ("online.forest");
     
-var scopeDropdown = $("#searchscope").val();
-
-delete scopeStrings[scopeDropdown];
+    var scopeDropdown = $("#searchscope").val();
     
-    if ($("table").is('.bibLinks')){
-        $(".bibLinks a").each(function(index, element) {
-	    for (var k in scopeStrings) {
-		for (var l in scopeStrings[k]) {
-		    if($(element).parent().html().indexOf(scopeStrings[k][l]) > -1) {
-		      $(element).parent().parent().hide()
+    var goodStrings = new Array ();
+    goodStrings.push(stringsToHide[scopeDropdown]);
+    
+    delete stringsToHide[scopeDropdown];
+    
+    var hiddenLinks = new Array ();
+	
+	if ($("table").is('.bibLinks')){
+	    for (var k in goodStrings) {
+		for (var l in goodStrings[k]) {
+		    if($(".bibLinks").html().indexOf(goodStrings[k][l]) > -1 ){
+			var scopedLinks = true;
 		    }
 		}
-	    }    
-        });
-    }  
-}
-
-
-$(document).ready(function () {
-    hideLinks();
+	    }
+	    for (var k in stringsToHide) {
+		for (var l in stringsToHide[k]) {
+		    $(".bibLinks a").each(function(index, element) {
+			if($(element).parent().html().indexOf(stringsToHide[k][l]) > -1) {
+			    $(element).parent().parent().hide()
+			    hiddenLinks.push(element);
+			}    
+		    });
+		}
+	    }
+	    if (scopedLinks != true && hiddenLinks.length > 0){
+		$(".bibLinks").hide();
+	    }
+	}  
+    }
+    
+    
+    $(document).ready(function () {
+	hideLinks();
 });
