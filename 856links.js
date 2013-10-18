989570ef80f25ffd51c9b72cf816d3abbae48e82
Megan Durham <megan@mobiusconsortium.org>
2013-10-18 15:35:50 -0500

    add cottey and evangel to 856 js

--- 856links.js
+++ 856links.js
@@ -7,10 +7,11 @@ function hideLinks(){
     stringsToHide[4] = new Array ("my.otc");
     stringsToHide[5] = new Array ("sbuniv", "SBU");
     stringsToHide[6] = new Array ("missouristate");
-    stringsToHide[8] = new Array ("cottey"); 
+    stringsToHide[8] = new Array ("cottey", "Cottey"); 
     stringsToHide[9] = new Array ("gobbc");
+    stringsToHide[10] = new Array ("Evangel");
     stringsToHide[12] = new Array ("online.forest");
-    
+
     var scopeDropdown = $("#searchscope").val();
     
     delete stringsToHide[scopeDropdown];
