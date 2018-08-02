5826de5319b609c80cbc98f704963b2946d81400
bmagic007 <blake@mobiusconsortium.org>
2018-08-02 15:07:58 -0500

    Tweaks to the URL sorter

--- sortUnscoped856s.js
+++ sortUnscoped856s.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
     
-    var labels = [];
-    var urls = [];
+    var all_links = [];
     $(".bibDisplayUrls").find("a").each(function(){
-        alert($(this).prop("href"));
+        all_links.push($(this).html + "_!!!!_" + $(this).prop("href"));
     });
+    
+    all_links.sort(function (a, b) {
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+    });
+    
+    var sorted_table = "<tbody><tr><th>Connect to</th></tr>";
+    var i;
+    for (i = 0; i < all_links.length; i++) {
+        var splits = all_links[i].split("_!!!!_");
+        sorted_table+="<tr align=\"center\">\n<td><a href=\"" + splits[1] + "\">" + splits[0] + "<\a><br></td></tr>\n";
+    } 
+    sorted_table+="<\tbody>\n";
+    
+    $(".bibDisplayUrls table").html(sorted_table);
 
 
 });
